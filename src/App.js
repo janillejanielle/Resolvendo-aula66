977041110166d0ec292d0ddf1c9541fdac6abd92
janillejanielle <janille.janielle2@gmail.com>
2022-07-13 23:31:00 -0300

    Resolvendo a aula66

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Todos from './TodoList/Todos'
+import TrafficLight from './TrafficLights/TrafficLight';
+import './TrafficLights/styles.css'
+import FetchGithubUsers from './API/FetchGithubUsers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {<Todos /> }
+      <TrafficLight initialValue={0} />
+      <TrafficLight initialValue={1} />
+      <FetchGithubUsers />
+      
     </div>
   );
 }
